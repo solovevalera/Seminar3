410cd3f47eafedd6fb797cbc036f9f72b0df6c13
valeria <leraalex20@mail.ru>
2022-08-17 17:00:01 +0300

    added task23

--- task20/Program.cs
+++ task20/Program.cs
@@ -1,12 +1,21 @@
-﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-// 3 -> 1, 8, 27
-// 5 -> 1, 8, 27, 64, 125
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("Ввести число N");
-double N = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ввести координаты точки А по х");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ввести координаты точки А по y");
+double yA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ввести координаты точки А по z");
+double zA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ввести координаты точки B по х");
+double xB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ввести координаты точки B по y");
+double yB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ввести координаты точки B по z");
+double zB= Convert.ToDouble(Console.ReadLine());
 
-for (int i = 1; i <= N; i++)
-{
-    double result = Math.Pow(i, 3);
-    Console.WriteLine(result);
+double res = Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2) + Math.Pow(zB - zA, 2));
+
+Console.WriteLine($"{res:f2}");
 }
